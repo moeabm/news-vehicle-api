80f675ba560f1c50b26a9fcb7708219981fdf7e8
exit <exit>
2015-01-31 12:02:10 -0800

    Initial Upload

---
+++ routes/gps.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var express = require('express');
+var router = express.Router();
+
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('vehicle.db');
+var check;
+
+
+db.serialize(function() {
+	db.run("CREATE TABLE if not exists vehicle (lat DECIMAL, lon DECIMAL, stamp DATETIME )");
+})
+
+var gps = {
+	current: {
+		lat: 32.0383+(Math.random()),
+		lon: -117.0384+(Math.random())
+	}
+}
+/* GET vehicle last gps. */
+router.get('/', function(req, res, next) {
+	var last_gps = db.each("SELECT lat, lon, stamp FROM vehicle ORDER BY stamp DESC LIMIT 1", function(err, row){
+    	console.log(row);
+  		res.json(row);
+	})
+});
+
+/* PUT vehicle last gps. */
+router.put('/', function(req, res, next) {
+	// res.json(gps);
+		//console.log(req)
+		db.run("INSERT INTO vehicle (lat, lon, stamp) VALUES (?, ?, ?)", [
+			 req.body.lat,
+			 req.body.lon,
+			 new Date()
+		] );
+		res.send('rew record Lat: '+ req.body.lat+', Lon: '+ req.body.lon);
+
+});
+
+module.exports = router;
