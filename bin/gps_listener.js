1187ca7562175d5d30f1f35fec5ca4b4a76ce42d
Andrew Moore <moeabm@yahoo.com>
2015-02-01 17:20:58 -0800

    first gpsd working

--- bin/gps_listener.js
+++ bin/gps_listener.js
@@ -6,18 +6,36 @@ var db = new sqlite3.Database('vehicle.db');
 var daemon = new gpsd.Daemon({
         program: 'gpsd',
         device: '/dev/ttyUSB0',
-        verbose: true
 });
 
 daemon.start(function() {
-    var listener = new gpsd.Listener();
+    var listener = new gpsd.Listener({
+	parse: true
+    });
+    listener.logger = console;
+    console.log("gps listening");
     
     listener.on('TPV', function (tpv) {
-        console.log(tpv);
+        //console.log(tpv);
+        //console.log("got data");
         //TODO add sql insert command
+	if(tpv["mode"] > 1){
+		db.run("INSERT INTO vehicle (lat, lon, stamp) VALUES (?, ?, ?)", [
+			 tpv["lat"],
+			 tpv["lat"],
+			 new Date()
+		] );
+	}
     });
     
     listener.connect(function() {
-        listener.watch();
+    	console.log('Connected');
+        listener.watch({ class: 'WATCH', json: true, nmea: false });
     });
+
+   // if parse is false, so raw data get emitted.
+   listener.on('raw', function(data) {
+	console.log(data);
+   });
+
 });
